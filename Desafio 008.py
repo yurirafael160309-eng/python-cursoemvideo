5c67e64bbf0a46a1dddcc1d3fb7281def08bd7a5
yurirafael160309-eng <yurirafael160309@gmail.com>
2026-05-23 13:21:27 -0300

    Medidas de metros transformados em outras medidas

---
+++ Desafio 008.py
@@ -0,0 +1,13 @@
+m = float(input('Um valor em metros: '))
+mm = m * 1000
+cm = m * 100
+dm = m * 10
+dam = m / 10
+hm = m / 100
+km = m / 1000
+print('A sua medida de metros pra Milímetros é {}mm'.format(mm))
+print('A sua medida de metros pra Centímetros é {}cm'.format(cm))
+print('A sua medida de metros pra Decimetro é {}dm'.format(dm))
+print('A sua medida de metros pra Decâmetro é {}dam'.format(dam))
+print('A sua medida de metros pra Hectômetro é {}hm'.format(hm))
+print('A sua medida de metros pra Quilômetros é {}km'.format(km))
